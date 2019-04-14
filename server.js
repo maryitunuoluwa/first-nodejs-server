576c98c9d383907fdb7fc89f33c9d4880f1e521e
marieitunuoluwa <maryoluwaseun246@yahoo.com>
2019-04-14 15:19:21 +0100

    used new concepts, added comments

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const path = require('path')
+/*
 
+const path = require('path');
+
+*/
 const express = require('express');
 
 const server = express();
 
 const PORT = 3000; 
 
-server.use('/', (req, res) => {
+server.use('/', (req, res, next) => {
     console.log("Server running...");
-    res.send('<h1>Saying Hello World again!!</h1>');
+    res.send('<h1>Saying Hello World. From mary\'s computer 1 </h1>');
+    // res.send("<h1>Saying Hello World. From mary's computer </h1>");
 })
 
 server.listen(PORT)
